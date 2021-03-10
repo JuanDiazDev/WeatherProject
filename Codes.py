19f415fa6b7966bfb1446a909962e388d2eaf799
Juan Diaz <juanmanuel32@ciencias.unam.mx>
2021-03-09 19:15:10 -0700

    Update Codes.py

--- Codes.py
+++ Codes.py
@@ -1,15 +1,24 @@
 import pandas
 
-
+'''
+Clase Codes para obtener códigos IATA por ciudad
+'''
 class Codes:
 
-    #Inicializamos un objeto Codes leyendo el csv que pase como parámetro y llenamos su diccionario
+    '''
+    Constructor de la clase. Inicializa un diccionario con los códigos IATA para más
+    de 57000 ciudades
+    :param codes: Un archivo csv con los códigos IATA por ciudad
+    '''
     def __init__(self, codes):
         IATA_CODES = pandas.read_csv(codes, dtype={'municipality': str, 'iata_code': str})
         self._CODES = {}
         for i in range(57420):
             self._CODES[IATA_CODES['iata_code'][i]] = IATA_CODES['municipality'][i]
 
-    #Regresa la propiedad CODES la cual es el diccionario con codigos IATA y la respectiva ciudad
+    '''
+    Método para obtener un diccionario con los códigos IATA por su ciudad
+    :return: Un diccionario con los códigos IATA y la respectiva ciudad
+    '''
     def get_codes(self):
         return self._CODES
