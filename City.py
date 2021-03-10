0971280d53f1d42e1463bb55ebf32848db9ba0fd
Juan Diaz <juanmanuel32@ciencias.unam.mx>
2021-03-09 19:14:34 -0700

    Update City.py

--- City.py
+++ City.py
@@ -1,44 +1,76 @@
 import pyowm
 
-
+'''
+Clase City para manejar la información del clima de una ciudad
+'''
 class City:
-    dic = {}
+    '''
+    diccionario para guardar el reporte del clima por ciudad
+    '''
+    weather_info = {}
 
+    '''
+    Constructor de la clase.
+    :param name: El nombre de la instancia
+    '''
     def __init__(self, name):
         self._name = name
 
+    '''
+    Cambia el nombre a una instancia de City
+    :param: El nuevo nombre para la instancia
+    '''
     def set_name(self, name):
         self._name = name
 
+    '''
+    Regresa el nombre de una instancia de City
+    :return: El nombre de una instancia de City
+    '''
     def get_name(self):
         return self._name
 
-    # Regresa una cadena para mostrar la informacion del clima
+    '''
+    Método para darle el formato que queremos al reporte del clima
+    :param temperature: Un diccionario con información sobre la temperatura
+    :param humidity: Nivel de humedad
+    :param status: El estado general del clima 
+    :return: Una cadena con el reporte del clima
+    '''
     @staticmethod
-    def format_weather(tem, humidity, status):
-        maxi = tem['temp_max']
-        mini = tem['temp_min']
-        return f'Weather: {status}, Temperature: Max: {maxi} Min: {mini}, Humidity: {humidity}'
+    def format_weather(temperature, humidity, status):
+        max_temperature = temperature['temp_max']
+        min_temperature = temperature['temp_min']
+        return f'Weather: {status}, Temperature: Max: {max_temperature} Min: {min_temperature}, Humidity: {humidity}'
 
-    # Solicita a OWM API el informe del clima
+    '''
+    Solicitamos el reporte del clima al API wrapper pyowm
+    :param name: Nombre de la ciudad para la que solicitaremos la info del clima
+    :return: True si hay un error
+    :except NotFoundError, TimeoutError
+    '''
     @staticmethod
-    def request_weather(dic, name):
-        owm = pyowm.OWM('Your_API_key')
-        mgr = owm.weather_manager()
+    def request_weather(report, name):
+        owm = pyowm.OWM('4f35f6733973b80abe4d8a18ded3a52e')
+        manager = owm.weather_manager()
         try:
-            weather_report = mgr.weather_at_place(name).weather
+            weather_report = manager.weather_at_place(name).weather
             temperature = weather_report.temperature('celsius')
             humidity = weather_report.humidity
             status = weather_report.detailed_status
-            dic[name] = City.format_weather(temperature, humidity, status)
+            report[name] = City.format_weather(temperature, humidity, status)
         except (pyowm.commons.exceptions.NotFoundError, pyowm.commons.exceptions.TimeoutError):
             return True
 
-    # Regresa el informe del clima de una instancia de la clase
+    '''
+    Método para solicitar desde fuera de la clase
+    el reporte del clima de una instancia de City
+    :return: El reporte del clima para una instancia de City
+    '''
     def get_weather(self):
-        if self._name in self.dic:
-            return self.dic[self._name]
-        err = City.request_weather(self.dic, self._name)
-        if err:
+        if self._name in self.weather_info:
+            return self.weather_info[self._name]
+        error = City.request_weather(self.weather_info, self._name)
+        if error:
             return 'Weather unavailable'
-        return self.dic[self._name]
+        return self.weather_info[self._name]
